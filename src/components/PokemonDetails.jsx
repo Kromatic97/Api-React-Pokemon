42fa6d75c147f75501bcfdc530e35a33d1dc2e98
Kromatic97 <rogiromero@gmail.com>
2022-08-31 14:51:02 -0400

    trabajo completo

--- src/components/PokemonDetails.jsx
+++ src/components/PokemonDetails.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
-const PokemonDetails = () => {
-const {name} = useParams ()
 
+const PokemonDetails = () => {
+  const [pokeInfo, setPokeInfo] = useState()
+  const {name} = useParams ()
+  
+  useEffect(() => {
+    const URL = `https://pokeapi.co/api/v2/pokemon/${name}/`
+    axios.get(URL)
+    .then(res => setPokeInfo(res.data))
+    .catch(err => console.log(err))
+  }, [])
+  
 
   return (
-    <h1>You Pokemon Select is: {name}</h1>
+    <article>
+      <h1>You Pokemon Select is: {name} </h1>
+      <img src={pokeInfo?.sprites.other['official-artwork'].
+      front_default} alt="" />
+      
+    </article>
   )
 }
 
